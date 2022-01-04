9dcb0ffbe95840946865460730385cc1e48098d7
JesusVillegas194 <j.villegascambero@gmail.com>
2022-01-04 14:49:00 -0600

    [Add] Scroll to top for mobile / tablet

---
+++ components/ScrollToTop.jsx
@@ -0,0 +1,36 @@
+import { useScrollTrigger, Zoom, Box } from "@mui/material";
+
+function ScrollToTop({ children, window }) {
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (e) => {
+    const anchor = (e.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+}
+
+export default ScrollToTop;
